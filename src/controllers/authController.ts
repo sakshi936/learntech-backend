4bcb7f870faf580abc4a65ed391f2bb63230204a
Ujjwal <utamrakar3@gmail.com>
2025-02-18 22:39:31 +0530

    feat: added user login and verification functionality

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,8 @@
 import bcrypt from 'bcryptjs';
 import { sendVerificationEmail } from '../helpers/sendVerificationMail';
-import { Request } from 'express';
+import { Request, Response } from 'express';
 import  UserModel  from '../models/userModel';
+import jwt from 'jsonwebtoken';
 
 
 
@@ -40,7 +41,7 @@ export const register = async (req:Request, res:any) => {
       } else {
           const hashedPassword = await bcrypt.hash(password, 10);
           const expiryDate = new Date();
-          expiryDate.setHours(expiryDate.getHours() + 1);
+          expiryDate.setHours(expiryDate.getHours() + 100);
 
           const newUser = new UserModel({
               username,
@@ -74,4 +75,81 @@ export const register = async (req:Request, res:any) => {
           message: "Error registering user"
       });
   }
+}
+
+export const verify = async (req:Request, res:any) => {
+    try {
+        const {username,code} = await req.body;
+        // console.log(username,code);
+        const decodedUsername = decodeURIComponent(username);
+        const user = await UserModel.findOne({username:decodedUsername});
+        if(!user){
+           return res.status(404).json({message:"User not found",success:false});
+        }
+        const isCodeValid = user.verifyCode === code;
+        const isCodeNotExpired = new Date() < new Date(user.verifyCodeExpiry);
+        if(isCodeValid && isCodeNotExpired){
+           user.isVerified = true;
+           await user.save();
+           return res.status(200).json({message:"User verified",success:true});
+        }else if(!isCodeValid){
+           return res.status(400).json({message:"Invalid code",success:false});}
+           else if(!isCodeNotExpired){
+              return res.status(400).json({message:"Code expired",success:false});
+           }
+
+     } catch (error) {
+        console.log(error);
+        return res.status(500).json({message:"Error",success:false});
+     }
+}
+
+export const login = async (req:Request, res:any) => {
+    try {
+        const { email, password } = req.body;
+        const user = await UserModel.findOne({ email });
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid credentials"
+            });
+        }   
+        if (!user.isVerified) {
+            return res.status(401).json({
+                success: false,
+                message: "User not verified"
+            });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid credentials"
+            });
+        }
+        const token = jwt.sign(
+            { 
+                userId: user._id,
+                email: user.email,
+                role: user.role
+            },
+            process.env.JWT_SECRET!,
+            { expiresIn: '24h' }
+        );
+        console.log(token)
+        console.log(user)
+        return res.status(200).json({
+            success: true,
+            message: "Login successful",
+            data: {
+                token
+            }
+        });
+    } catch (error) {
+        console.error('Error logging in user', error);
+        return res.status(500).json({
+            success: false,
+            message: "Error logging in user"
+        });
+    }
 }
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
 import { Router, Response, Request } from "express";
-import { register } from "../controllers/authController";
+import { register,login,verify } from "../controllers/authController";
 
 
 const router = Router();
 
 router.post("/register",register)
+router.post("/login", login)
+router.post("/verify", verify)
 
 export default router;
